848ac9c4a2e9581053ea5176f16233869616b3e0
Kim Taejin <rlaxowls0504@gmail.com>
2023-06-23 18:48:38 +0900

    sequelize migrations 작성
    ERD에 맞게 migrations, models 작성

---
+++ models/post.js
@@ -0,0 +1,69 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, {
+        targetKey: "userId",
+        foreignKey: "UserId",
+      });
+
+      this.hasMany(models.Comments, {
+        sourceKey: "postId",
+        foreignKey: "PostId",
+      });
+    }
+  }
+  Post.init(
+    {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      UserId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "User",
+          key: "userId",
+        },
+        onDelete: "CASCADE",
+      },
+      nickname: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
+  return Post;
+};
---
+++ models/user.js
@@ -0,0 +1,57 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Posts, {
+        sourceKey: "userId",
+        foreignKey: "UserId",
+      });
+
+      this.hasMany(models.Comments, {
+        sourceKey: "userId",
+        foreignKey: "UserId",
+      });
+    }
+  }
+  User.init(
+    {
+      userId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      nickname: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
